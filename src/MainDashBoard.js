239606a50b79535850803754174af01211ecea87
seunghyun <masterhyun1207@nate.com>
2023-05-25 15:04:57 +0900

    2023-05-25 React Project Nav Update 001

--- src/MainDashBoard.js
+++ src/MainDashBoard.js
@@ -8,6 +8,8 @@ function MainDashBoard(){
         <div className="mainWrap">
             {/* 사이드바 */}
             <SideBar/>
+            {/* 컨텐츠 */}
+
         </div>
     )
 
--- src/SideBar.js
+++ src/SideBar.js
@@ -15,11 +15,23 @@ function SideBar(){
                 <div className="youtube sidecont">
                     <Link to="https://www.youtube.com/channel/UCHC0mo3JMGpzGeV_mLKQ9ug" target="_blank" style={{ textDecoration: 'none' }}>Youtube</Link>
                 </div>
-                <div className="trip">
-                    <Link to="/trip" style={{textDecoration: 'none'}}>Trip</Link>
+                <div className="instagram sidecont">
+                    <Link to="https://instagram.com/s12.hyun07?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" style={{textDecoration: 'none'}}>Instagram</Link>
+                </div>
+                <div className="trip sidecont">
+                    <Link to="/Trip" style={{textDecoration: 'none'}}>Trip</Link>
+                </div>
+                <div className="game sidecont">
+                    <Link to="/Game" style={{textDecoration: 'none'}}>Game</Link>
+                </div>
+                <div className="study sidecont">
+                    <Link to="/Study" style={{textDecoration: 'none'}}>Study</Link>
                 </div>
                 <div className="setting sidecont">
-                    <Link to="/setting" style={{textDecoration: 'none'}}>Setting</Link>
+                    <Link to="/Setting" style={{textDecoration: 'none'}}>Setting</Link>
+                </div>
+                <div className="portfolio sidecont">
+                    <Link to="/PortFolio" style={{textDecoration: 'none'}}>PortFolio</Link>
                 </div>
             </div>
         </div>
